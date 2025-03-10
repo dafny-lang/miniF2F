b3b6d150f05786c5ec09a38ea0f6324b5b91413f
zetzschest <stefanze@amazon.com>
2025-03-10 16:17:54 +0000

    update results.py for ollama and bedrock

--- eval/results.py
+++ eval/results.py
@@ -8,17 +8,20 @@ import shutil
 import os
 from botocore.exceptions import ClientError
 import time
+from ollama import chat
+from ollama import ChatResponse
+import re
 
 DAFNY_PATH = "/Users/stefanze/Downloads/dafny-4.10.0/dafny"
 CLIENT = boto3.client("bedrock-runtime",  region_name="us-east-1")
-SONNET_MODEL_ID = "arn:aws:bedrock:us-east-1:730335450364:inference-profile/us.anthropic.claude-3-5-sonnet-20241022-v2:0"
-#DEEPSEEK_MODEL_ID =
-LLAMA_MODEL_ID = "arn:aws:bedrock:us-east-1:730335450364:inference-profile/us.meta.llama3-3-70b-instruct-v1:0"
+SONNET_BEDROCK_MODEL_ID = "arn:aws:bedrock:us-east-1:730335450364:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+LLAMA_BEDROCK_MODEL_ID = "arn:aws:bedrock:us-east-1:730335450364:inference-profile/us.meta.llama3-3-70b-instruct-v1:0"
+NOVA_BEDROCK_ID = "arn:aws:bedrock:us-east-1:730335450364:inference-profile/us.amazon.nova-pro-v1:0"
 UTILS_FILE_PATH = "./dataset/utils.dfy"
 VALID_FOLDER_PATH = "./dataset/valid"
 TEST_FOLDER_PATH = "./dataset/test"
 RESULTS_PATH = "results"
-N = 4
+N = 5
 TEMPERATURE = 0.5
 TOP_P = 0.9
 MAX_GEN_LEN = 2048
@@ -31,13 +34,15 @@ def write_test_results(results: List[Dict[str, Any]], results_file_path: Path, i
   with results_file_path.open('w') as file:
     json.dump(results, file, indent=indent)
 
-def evaluate(type: str, model: str) -> None:
+def evaluate(type: str, model: str, service: str) -> None:
   if type == "valid":
     folder_path = Path(VALID_FOLDER_PATH)
   if type == "test":
     folder_path = Path(TEST_FOLDER_PATH)
   results = []
   results_file_path = Path(f"{RESULTS_PATH}/{model}/{type}/results.json")
+  with open(UTILS_FILE_PATH, 'r') as file:
+    utils_file_content = file.read()
   # Loop over every file in folder_path
   for incomplete_dafny_file in folder_path.iterdir():
     if incomplete_dafny_file.is_file():
@@ -53,6 +58,7 @@ def evaluate(type: str, model: str) -> None:
         lines = file.readlines()
       lines = lines[:-1]
       incomplete_dafny_file_content = ''.join(lines)
+      prompt = "The Dafny file 'utils.dfy' has the following content: \n\n" + utils_file_content + "\n\n\n Please append to the following Dafny file so that it verifies: \n\n" + incomplete_dafny_file_content + "\n\n Only return the part that you want to append. Do not return anything else."
       # Set verified to false
       verified = False
       # Loop until verified or i >= N
@@ -62,19 +68,14 @@ def evaluate(type: str, model: str) -> None:
         if model == "dafny":
           completion = "{}"
           i = N
-        # Sonnet completion
-        if model == "sonnet":
+        else:
           if i == 0:
             completion = "{}"
           else:
-            completion = call_bedrock(incomplete_dafny_file_content, SONNET_MODEL_ID)
-            time.sleep(7) # sleep to avoid throttling exception
-        # Llama completion
-        if model == "llama":
-          if i == 0:
-            completion = "{}"
-          else:
-            completion =  call_bedrock(incomplete_dafny_file_content, LLAMA_MODEL_ID)
+            if service == "bedrock":
+              completion = call_bedrock(prompt, model)
+            if service == "ollama":
+              completion = call_ollama(prompt, model)
         # Add completion
         complete_dafny_file_content = incomplete_dafny_file_content + str(completion)
         # Write completed file
@@ -110,14 +111,8 @@ def evaluate(type: str, model: str) -> None:
           results.append(create_test_result(incomplete_dafny_file_path, f"failed after {N} iterations"))
       write_test_results(results, results_file_path)
 
-
-def call_bedrock(incomplete_dafny_file_content: str, model_id: str) -> str:
-  with open(UTILS_FILE_PATH, 'r') as file:
-    utils_file_content = file.read()
-
-  prompt = "The Dafny file 'utils.dfy' has the following content: \n\n" + utils_file_content + "\n\n\n Please append to the following Dafny file so that it verifies: \n\n" + incomplete_dafny_file_content + "\n\n Only return the part that you want to append. Do not return anything else."
-
-  if model_id == SONNET_MODEL_ID:
+def call_bedrock(prompt: str, model_id: str) -> str:
+  if model_id == SONNET_BEDROCK_MODEL_ID:
     body = json.dumps({
       "anthropic_version": "bedrock-2023-05-31",
       "max_tokens": MAX_GEN_LEN,
@@ -143,7 +138,7 @@ def call_bedrock(incomplete_dafny_file_content: str, model_id: str) -> str:
     response_body = json.loads(response.get('body').read())
     response_text = response_body["content"][0]["text"]
   
-  if model_id == LLAMA_MODEL_ID:
+  if model_id == LLAMA_BEDROCK_MODEL_ID:
     formatted_prompt = f"""
     <|begin_of_text|><|start_header_id|>user<|end_header_id|>
     {prompt}
@@ -165,30 +160,77 @@ def call_bedrock(incomplete_dafny_file_content: str, model_id: str) -> str:
     response_body = json.loads(response.get('body').read())
     response_text = response_body["generation"]
 
+  if model_id == NOVA_BEDROCK_ID:
+    body = json.dumps({
+      "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "text": prompt
+              }
+            ]
+          }
+        ],
+      "inferenceConfig": {
+        "maxTokens": MAX_GEN_LEN,
+        "temperature": TEMPERATURE,
+        "topP": TOP_P
+      }
+    })
+    try:
+      response = CLIENT.invoke_model(body=body, modelId=model_id)
+    except (ClientError, Exception) as e:
+      print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+      exit(1)
+    response_body = json.loads(response.get('body').read())
+    response_text = response_body["output"]["message"]["content"][0]["text"]
+
   return response_text
 
+def call_ollama(prompt: str, model_id: str) -> str:
+  response: ChatResponse = chat(
+    model=model_id, 
+    messages=[
+      {
+        'role': 'user',
+        'content': prompt,
+      },
+    ], 
+    options={
+      'temperature': TEMPERATURE,
+      'top_p': TOP_P,
+      'num_ctx': MAX_GEN_LEN
+    }
+  )
+  response_content = response.message.content
+  response_content = re.sub(r"<think>.*?</think>\n?", "", response_content, flags=re.DOTALL)
+  return response_content                        
 
 def verify_dafny_file(file, silent=False):
-    try:
-      s = subprocess.run(
-        [DAFNY_PATH, "verify", file, *(['--show-snippets', 'False'] if silent else [])],
-        shell=False,
-        capture_output=True,
-        timeout=15,
-      )
-      
-      output = s.stdout.decode('utf-8') + s.stderr.decode('utf-8')
-      return output, (
-        s.returncode == 0 and \
-        "verified, 0 errors" in output and \
-        "File contains no code" not in output
-      )
-    except subprocess.TimeoutExpired:
-        return "Timed out after 15 seconds", False
+  try:
+    s = subprocess.run(
+      [DAFNY_PATH, "verify", file, *(['--show-snippets', 'False'] if silent else [])],
+      shell=False,
+      capture_output=True,
+      timeout=15,
+    )
+    
+    output = s.stdout.decode('utf-8') + s.stderr.decode('utf-8')
+    return output, (
+      s.returncode == 0 and \
+      "verified, 0 errors" in output and \
+      "File contains no code" not in output
+    )
+  except subprocess.TimeoutExpired:
+      return "Timed out after 15 seconds", False
     
 def main() -> None:
-  evaluate("test", "dafny")
-  evaluate("valid", "dafny")
+  #evaluate("valid", "codestral", "ollama")
+  #evaluate("valid", "llama3.2", "ollama")
+  #evaluate("valid", LLAMA_BEDROCK_MODEL_ID, "bedrock")
+  #evaluate("valid", NOVA_BEDROCK_ID, "bedrock")
+  evaluate("valid", SONNET_BEDROCK_MODEL_ID, "bedrock")
 
 if __name__ == "__main__":
   main()
